c7168672ce4f533feb99f4b9d158e097dbc26325
Pablo Cordero <dimenwarper@gmail.com>
2021-11-14 20:21:01 -0800

    Lots of reorg

--- shuffle_and_split.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-shuf data/pos_processed_gnps.txt > data/pos_processed_gnps_shuffled.txt
-tail -n +137001 data/pos_processed_gnps_shuffled.tsv > data/pos_processed_gnps_shuffled_validation.tsv
-head -n 137000 data/pos_processed_gnps_shuffled.tsv > data/pos_processed_gnps_shuffled_train.tsv
